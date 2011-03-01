f78f6bea9c4bd6cfd3b632384210ed08d3b351e4
Jeremy Morse <jmorse+git@studentrobotics.org>
2011-03-01 19:33:12 +0000

    Add some sric opening and searching-for-power-board code.

--- srinput.c
+++ srinput.c
@@ -1,16 +1,45 @@
 #include <stdio.h>
+#include <unistd.h>
 
 #include <X11/Xlib.h>
 #include <X11/keysym.h>
 #include <X11/extensions/XTest.h>
 
+#include <sric.h>
+
+Display *d;
+sric_context ctx;
+
 int
 main(int argc, char **argv)
 {
-	Display *d;
+	sric_frame frame;
+	sric_device *device;
 
 	d = XOpenDisplay(NULL);
-	printf("Ohai, d is %p\n", d);
+	if (d == NULL)
+		abort();
+
+	ctx = sric_init();
+	if (ctx == NULL)
+		abort();
+
+	/* Find power board address */
+	device = NULL;
+	do {
+		device = sric_enumerate_devices(ctx, device);
+		if (device->type == SRIC_CLASS_POWER)
+			break;
+	} while (device != NULL)
+
+	if (device == NULL) {
+		fprintf(stderr, "Couldn't find a plugged in power board\n");
+		abort();
+	}
+
+	/* Signal we want info on button presses */
+#error bees
+
 	XTestFakeKeyEvent(d, XK_Up, True, 0);
 	XTestFakeKeyEvent(d, XK_Up, False, 20);
 	XCloseDisplay(d);
